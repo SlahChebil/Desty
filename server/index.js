6386d5ab4ebf97d4659dbe7abe776b660aeffcef
Sleh Chebil <sleh.chebil.sc@gmail.com>
2021-12-01 09:47:18 +0100

    init server config(COntrollers,routes,models)

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+// App Routes
+app.use('/posts',postRoutes);
+
+//COnnection URL
+const URL='mongodb+srv://Gzome:WERmz42hLqYZaA3@cluster0.cf0fy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT= process.env.PORT || 5000;
+mongoose.connect(URL,{useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> app.listen(PORT,()=>console.log(`Srever running on port: ${PORT}`)))
+    .catch((err)=> console.log(err));
+
